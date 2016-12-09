718fb7e780816f77ce6a30e446214fa20501599d
jonatalamantes <jonathan_s_pisis@hotmail.com>
2016-12-09 12:17:40 -0600

    Creacion de metodos en la clase base

--- code/Alumno.php
+++ code/Alumno.php
@@ -14,4 +14,250 @@
         private $fechaRegistro;
         private $tipo;
 
+        /**
+         * Constructor de la Clase
+         */
+        function __construct($id = "", $nombres = "", $apellido_paterno = "", 
+                            $apellido_materno = "", $password = "", $activo = "", 
+                            $fecha_registro = "", $tipo = "")
+        {
+            $this->id              = $id;
+            $this->nombres         = $nombres;
+            $this->apellidoPaterno = $apellido_paterno;
+            $this->apellidoMaterno = $apellido_materno;
+            $this->password        = $password;
+            $this->activo          = $activo;
+            $this->fechaRegistro   = $fecha_registro;
+            $this->tipo            = $tipo;
+        }
+        
+        /**
+         * Retorna un Array del Objeto
+         * 
+         * @return [array] [Array Asociativo Resultante]
+         */
+        public function toArray()
+        {
+            $array = array();
+        
+            if ($this !== null)
+            {
+                $array["id"]               = $this->getId();
+                $array["nombres"]          = $this->getNombres();
+                $array["apellido_paterno"] = $this->getApellidoPaterno();
+                $array["apellido_materno"] = $this->getApellidoMaterno();
+                $array["password"]         = $this->getPassword();
+                $array["activo"]           = $this->getActivo();
+                $array["fecha_registro"]   = $this->getFechaRegistro();
+                $array["tipo"]             = $this->getTipo();
+            }
+        
+            return $array;
+        }
+        
+        /**
+         * Toma los datos de un Array para el Objeto
+         * 
+         * @param  array  $array [Array Entrante]
+         */
+        public function fromArray($array = array())
+        {
+            if (!empty($array))
+            {
+                $this->setId($array["id"]);
+                $this->setNombres($array["nombres"]);
+                $this->setApellidoPaterno($array["apellido_paterno"]);
+                $this->setApellidoMaterno($array["apellido_materno"]);
+                $this->setPassword($array["password"]);
+                $this->setActivo($array["activo"]);
+                $this->setFechaRegistro($array["fecha_registro"]);
+                $this->setTipo($array["tipo"]);
+            }
+        }
+        
+        /**
+         * Calculo para saber que tan diferente es un objeto de otro
+         * 
+         * @param  Alumno $obj [Objeto con el que se comparara]
+         * @return [float]     [Disimilitud entre los dos objetos]
+         */
+        public function disimilitud($obj = null)
+        {
+            if ($obj === null)
+            {
+                return -1;
+            }
+        
+            $disimilitud = 0;
+            $numerador   = 0;
+            $denominador = 0;
+            
+            if ($obj->getNombres() != $this->getNombres())
+            {
+                $numerador += 1;
+            }
+        
+            $denominador += 1;
+        
+            if ($obj->getApellidoPaterno() != $this->getApellidoPaterno())
+            {
+                $numerador += 1;
+            }
+        
+            $denominador += 1;
+        
+            if ($obj->getApellidoMaterno() != $this->getApellidoMaterno())
+            {
+                $numerador += 1;
+            }
+        
+            $denominador += 1;
+        
+            if ($obj->getPassword() != $this->getPassword())
+            {
+                $numerador += 1;
+            }
+        
+            $denominador += 1;
+        
+            if ($obj->getTipo() != $this->getTipo())
+            {
+                $numerador += 1;
+            }
+        
+            $denominador += 1;
+        
+            $disimilitud = (float)($numerador/$denominador);
+            return $disimilitud;
+        }
+        
+        /**
+         * Gets the value of id
+         */
+        public function getId()
+        {
+            return $this->id;
+        }
+        
+        /**
+         * Sets the value of id
+         */
+        public function setId($id)
+        {
+            $this->id = $id;
+        }
+        
+        /**
+         * Gets the value of nombres
+         */
+        public function getNombres()
+        {
+            return $this->nombres;
+        }
+        
+        /**
+         * Sets the value of nombres
+         */
+        public function setNombres($nombres)
+        {
+            $this->nombres = $nombres;
+        }
+        
+        /**
+         * Gets the value of apellidoPaterno
+         */
+        public function getApellidoPaterno()
+        {
+            return $this->apellidoPaterno;
+        }
+        
+        /**
+         * Sets the value of apellidoPaterno
+         */
+        public function setApellidoPaterno($apellidoPaterno)
+        {
+            $this->apellidoPaterno = $apellidoPaterno;
+        }
+        
+        /**
+         * Gets the value of apellidoMaterno
+         */
+        public function getApellidoMaterno()
+        {
+            return $this->apellidoMaterno;
+        }
+        
+        /**
+         * Sets the value of apellidoMaterno
+         */
+        public function setApellidoMaterno($apellidoMaterno)
+        {
+            $this->apellidoMaterno = $apellidoMaterno;
+        }
+        
+        /**
+         * Gets the value of password
+         */
+        public function getPassword()
+        {
+            return $this->password;
+        }
+        
+        /**
+         * Sets the value of password
+         */
+        public function setPassword($password)
+        {
+            $this->password = $password;
+        }
+        
+        /**
+         * Gets the value of activo
+         */
+        public function getActivo()
+        {
+            return $this->activo;
+        }
+        
+        /**
+         * Sets the value of activo
+         */
+        public function setActivo($activo)
+        {
+            $this->activo = $activo;
+        }
+        
+        /**
+         * Gets the value of fechaRegistro
+         */
+        public function getFechaRegistro()
+        {
+            return $this->fechaRegistro;
+        }
+        
+        /**
+         * Sets the value of fechaRegistro
+         */
+        public function setFechaRegistro($fechaRegistro)
+        {
+            $this->fechaRegistro = $fechaRegistro;
+        }
+        
+        /**
+         * Gets the value of tipo
+         */
+        public function getTipo()
+        {
+            return $this->tipo;
+        }
+        
+        /**
+         * Sets the value of tipo
+         */
+        public function setTipo($tipo)
+        {
+            $this->tipo = $tipo;
+        }
+        
+    }
 ?>
--- src/ExportadorPHP.java
+++ src/ExportadorPHP.java
@@ -53,6 +53,36 @@ public class ExportadorPHP
 		
 		writer.println(msj);
 	}
+	
+	public static int getTamEspacios(String comparar, Clase clase, boolean camel)
+	{
+		int valMayor = -1;
+		
+		for (int i = 0; i < clase.getAtributos().size(); i++)
+		{
+			String nombreClase = clase.getAtributos().get(i).getNombre();
+			
+			if (camel)
+			{
+				nombreClase = toCamelCase(nombreClase);
+			}
+			
+			if (nombreClase.length() > valMayor)
+			{
+				valMayor = nombreClase.length();
+			}
+		}
+		
+		if (camel)
+		{
+			return Math.abs(toCamelCase(comparar).length() - valMayor);
+		}
+		else
+		{
+			return Math.abs(comparar.length() - valMayor);
+		}
+		
+	}
 
 	public static void exportarClase(Clase clase)
 	{
@@ -65,6 +95,7 @@ public class ExportadorPHP
 		{
 			writer = new PrintWriter(path, "UTF-8");
 
+			//Creamos la parte de arriba de la clase
 			writer.println("<?php ");
 			writer.println();
 			espacioTabulador++;
@@ -75,12 +106,216 @@ public class ExportadorPHP
 			imprimir("{", writer, espacioTabulador);
 			espacioTabulador++;
 			
+			//Creamos los atributos
 			for (int i = 0; i < clase.getAtributos().size(); i++)
 			{
 				imprimir("private $" + toCamelCase(clase.getAtributos().get(i).getNombre()) + ";", writer, espacioTabulador);
 			}
 			
 			writer.println("");
+			
+			//Creamos el constructor
+			imprimir("/**", writer, espacioTabulador);
+			imprimir(" * Constructor de la Clase", writer, espacioTabulador);
+			imprimir(" */", writer, espacioTabulador);
+			
+			String cadAcumulada = "function __construct(";
+			boolean primero = false;
+			for (int i = 0; i < clase.getAtributos().size(); i++)
+			{
+				AtributoClase atributoActual = clase.getAtributos().get(i);
+				String defecto = "\"\"";
+				String defectoPrev = atributoActual.getDefecto().replaceAll("\"", "");
+				
+				if (defectoPrev.equals(""))
+				{
+					defecto = "\""+defectoPrev+"\"";
+				}
+				
+				if (i == 0)
+				{
+					primero = true;
+					cadAcumulada += "$" + atributoActual.getNombre() + " = " + defecto + ", "; 
+				}
+				else if (i % 3 == 0)
+				{
+					if (i == clase.getAtributos().size()-1)
+					{
+						cadAcumulada = cadAcumulada.substring(0, cadAcumulada.length()-2);
+						cadAcumulada += ")";
+					}
+					
+					if (primero)
+					{
+						primero = false;
+						imprimir(cadAcumulada, writer, espacioTabulador);
+					}
+					else
+					{
+						imprimir(cadAcumulada, writer, espacioTabulador+5);
+					}		
+					
+					cadAcumulada = "";
+					cadAcumulada += "$" + atributoActual.getNombre() + " = " + defecto + ", ";
+				}
+				else
+				{
+					cadAcumulada += "$" + atributoActual.getNombre() + " = " + defecto + ", ";
+				}
+			}
+			
+			if (!cadAcumulada.equals(""))
+			{
+				cadAcumulada = cadAcumulada.substring(0, cadAcumulada.length()-2);
+				
+				if (primero)
+				{
+					primero = false;
+					imprimir(cadAcumulada + ")", writer, espacioTabulador);
+				}
+				else
+				{
+					imprimir(cadAcumulada + ")", writer, espacioTabulador+5);
+				}					
+			}
+			
+			imprimir ("{", writer, espacioTabulador);
+			
+			for (int i = 0; i < clase.getAtributos().size(); i++)
+			{
+				String cad = "$this->" + toCamelCase(clase.getAtributos().get(i).getNombre());
+				int tam = getTamEspacios(clase.getAtributos().get(i).getNombre(), clase, true);
+				
+				for (int j = 0; j < tam; j++)
+				{
+					cad += " ";
+				}
+				
+				cad += " = $" + clase.getAtributos().get(i).getNombre() + ";";  
+				
+				imprimir(cad, writer, espacioTabulador+1);
+			}
+			
+			imprimir("}", writer, espacioTabulador);
+			imprimir("", writer, espacioTabulador);
+			
+			//Generamos el metodo de ToArray
+	        imprimir("/**", writer, espacioTabulador);
+	        imprimir(" * Retorna un Array del Objeto", writer, espacioTabulador);
+	        imprimir(" * ", writer, espacioTabulador);
+	        imprimir(" * @return [array] [Array Asociativo Resultante]", writer, espacioTabulador);
+	        imprimir(" */", writer, espacioTabulador);
+	        imprimir("public function toArray()", writer, espacioTabulador);
+	        imprimir("{", writer, espacioTabulador);
+	        imprimir("$array = array();", writer, espacioTabulador+1);
+	        imprimir("", writer, espacioTabulador);
+	        imprimir("if ($this !== null)", writer, espacioTabulador+1);
+	        imprimir("{", writer, espacioTabulador+1);
+			for (int i = 0; i < clase.getAtributos().size(); i++)
+			{
+				String cad = "$array[\"" + clase.getAtributos().get(i).getNombre() + "\"]";
+				int tam = getTamEspacios(clase.getAtributos().get(i).getNombre(), clase, false);
+				
+				for (int j = 0; j < tam; j++)
+				{
+					cad += " ";
+				}
+				
+				cad += " = $this->" + toCamelCase("get_" + clase.getAtributos().get(i).getNombre()) + "();";  
+				
+				imprimir(cad, writer, espacioTabulador+2);
+			}
+			imprimir("}", writer, espacioTabulador+1);
+			imprimir("", writer, espacioTabulador);
+			imprimir("return $array;", writer, espacioTabulador+1);
+			imprimir("}", writer, espacioTabulador);
+			imprimir("", writer, espacioTabulador);
+			
+			//Creamos el metodo fromArray
+			imprimir("/**", writer, espacioTabulador);
+			imprimir(" * Toma los datos de un Array para el Objeto", writer, espacioTabulador);
+			imprimir(" * ", writer, espacioTabulador);
+			imprimir(" * @param  array  $array [Array Entrante]", writer, espacioTabulador);
+			imprimir(" */", writer, espacioTabulador);
+			imprimir("public function fromArray($array = array())", writer, espacioTabulador);
+			imprimir("{", writer, espacioTabulador);
+			imprimir("if (!empty($array))", writer, espacioTabulador+1);
+			imprimir("{", writer, espacioTabulador+1);
+			for (int i = 0; i < clase.getAtributos().size(); i++)
+			{
+				String cad = "$this->" + toCamelCase("set_" + clase.getAtributos().get(i).getNombre()) + "(";
+				cad += "$array[\"" + clase.getAtributos().get(i).getNombre() + "\"]);";  
+				
+				imprimir(cad, writer, espacioTabulador+2);
+			}
+			imprimir("}", writer, espacioTabulador+1);
+			imprimir("}", writer, espacioTabulador);
+			imprimir("", writer, espacioTabulador);
+			
+			//Creamos el metodo de la disimilitud
+			imprimir("/**", writer, espacioTabulador);
+			imprimir(" * Calculo para saber que tan diferente es un objeto de otro", writer, espacioTabulador);
+			imprimir(" * ", writer, espacioTabulador);
+			imprimir(" * @param  Alumno $obj [Objeto con el que se comparara]", writer, espacioTabulador);
+			imprimir(" * @return [float]     [Disimilitud entre los dos objetos]", writer, espacioTabulador);
+			imprimir(" */", writer, espacioTabulador);
+			imprimir("public function disimilitud($obj = null)", writer, espacioTabulador);
+			imprimir("{", writer, espacioTabulador);
+			imprimir("if ($obj === null)", writer, espacioTabulador+1);
+			imprimir("{", writer, espacioTabulador+1);
+			imprimir("return -1;", writer, espacioTabulador+2);
+			imprimir("}", writer, espacioTabulador+1);
+			imprimir("", writer, espacioTabulador);
+			imprimir("$disimilitud = 0;", writer, espacioTabulador+1);
+			imprimir("$numerador   = 0;", writer, espacioTabulador+1);
+			imprimir("$denominador = 0;", writer, espacioTabulador+1);
+			imprimir("", writer, espacioTabulador+1);
+			for (int i = 0; i < clase.getAtributos().size(); i++)
+			{
+				if (clase.getAtributos().get(i).isDisimil())
+				{
+					String getter = toCamelCase("get_" + clase.getAtributos().get(i).getNombre());
+					
+					imprimir("if ($obj->" + getter + "() != $this->" + getter + "())", writer, espacioTabulador+1);
+					imprimir("{", writer, espacioTabulador+1);
+					imprimir("$numerador += 1;", writer, espacioTabulador+2);
+					imprimir("}", writer, espacioTabulador+1);
+					imprimir("", writer, espacioTabulador);
+					imprimir("$denominador += 1;", writer, espacioTabulador+1);
+					imprimir("", writer, espacioTabulador);
+				}
+			}
+            imprimir("$disimilitud = (float)($numerador/$denominador);", writer, espacioTabulador+1);
+            imprimir("return $disimilitud;", writer, espacioTabulador+1);
+            imprimir("}", writer, espacioTabulador);
+            imprimir("", writer, espacioTabulador);
+
+            //Creamos los getters y setters
+            for (int i = 0; i < clase.getAtributos().size(); i++)
+            {
+            	String nombre = toCamelCase(clase.getAtributos().get(i).getNombre());
+            	String nombreGetter = toCamelCase("get_" + clase.getAtributos().get(i).getNombre());
+            	String nombreSetter = toCamelCase("set_" + clase.getAtributos().get(i).getNombre());
+            	
+            	imprimir("/**", writer, espacioTabulador);
+            	imprimir(" * Gets the value of " + nombre, writer, espacioTabulador);
+            	imprimir(" */", writer, espacioTabulador);
+            	imprimir("public function " + nombreGetter +"()", writer, espacioTabulador);
+            	imprimir("{", writer, espacioTabulador);
+            	imprimir("return $this->" + nombre + ";", writer, espacioTabulador+1);
+            	imprimir("}", writer, espacioTabulador);
+            	imprimir("", writer, espacioTabulador);
+            	imprimir("/**", writer, espacioTabulador);
+            	imprimir(" * Sets the value of " + nombre, writer, espacioTabulador);
+            	imprimir(" */", writer, espacioTabulador);
+            	imprimir("public function " + nombreSetter + "($" + nombre + ")", writer, espacioTabulador);
+            	imprimir("{", writer, espacioTabulador);
+            	imprimir("$this->" + nombre + " = $" + nombre + ";", writer, espacioTabulador+1);
+            	imprimir("}", writer, espacioTabulador);
+            	imprimir("", writer, espacioTabulador);
+            }
+            
+            imprimir("}", writer, espacioTabulador-1);
 			writer.println("?>");
 			
 			writer.close();
